2ed996caf33e61a8f5e3b910d45afe5fbfc02722
adarshadhunti <adarshadhunti@gmail.com>
2022-11-09 09:17:03 +0530

    qa

--- src/test/java/resources/ExtentReporterNG.java
+++ src/test/java/resources/ExtentReporterNG.java
@@ -13,7 +13,7 @@ public class ExtentReporterNG {
     {
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
         String path =System.getProperty("user.dir")+"\\reports\\"+ timeStamp +"\\index.html";
-        String path1 =System.getProperty("user.dir")+"\\Maven-reports\\"+ "\\Extent.html";
+        String path1 =System.getProperty("user.dir")+"\\target\\"+ "\\Extent.html";
         ExtentSparkReporter reporter = new ExtentSparkReporter(path);
         ExtentSparkReporter reporter1 = new ExtentSparkReporter(path1);
         reporter.config().setReportName("Web Automation Results");
