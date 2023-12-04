9b9c6c45e303712d3bf00420c501ce588f8f4ff7
adarshadhunti <83895220+adarshadhunti@users.noreply.github.com>
2023-12-04 20:21:38 +0530

    Update TC_HM_003_parameterize_excel_util1.java

--- src/test/java/testcases/TC_HM_003_parameterize_excel_util1.java
+++ src/test/java/testcases/TC_HM_003_parameterize_excel_util1.java
@@ -11,6 +11,7 @@ public class TC_HM_003_parameterize_excel_util1 extends BaseTest {
     //@author='Adarsh'
     //hello
     //hello1
+    //hello2
     @Test(dataProvider = "getData4")
     public void TC_HM_003_parameterize_excel_util1(String city, String no_of_brands) throws IOException {
         HomePage hp = new HomePage(driver);
