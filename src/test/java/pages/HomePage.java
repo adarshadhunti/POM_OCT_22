5e72a2dc904049c43455b2a418d6929e7624e048
Adarsh <adarshadhunti@gmail.com>
2025-12-27 14:40:41 +0400

    qa

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.io.IOException;
 import java.util.List;
@@ -30,6 +31,7 @@ public class HomePage extends BasePage {
 
     public HomePage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public void verifyMenus() {
