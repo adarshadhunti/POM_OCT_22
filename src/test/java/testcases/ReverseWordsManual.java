d004b872f13fa77cdf0e3492ebc189205994e5ec
Adarsh <adarshadhunti@gmail.com>
2026-01-09 13:41:21 +0400

    qa

--- src/test/java/testcases/ReverseWordsManual.java
+++ src/test/java/testcases/ReverseWordsManual.java
@@ -26,6 +26,6 @@ public class ReverseWordsManual {
         System.out.println("Original sentence: " + input);
         //System.out.println("Reversed words: " + output);
         System.out.println("Originall sentence: " + input);
-
+        //NOTESREMOVED
     }
 }
