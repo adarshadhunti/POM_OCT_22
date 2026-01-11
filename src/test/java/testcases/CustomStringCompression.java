69d3c6d114662ad04e3a710218ea35169b61c6b1
Adarsh <adarshadhunti@gmail.com>
2026-01-11 07:14:20 +0400

    Refactor compressString: clearer logic and explicit cases (1,2,>=3) and comments

---
+++ src/test/java/testcases/CustomStringCompression.java
@@ -0,0 +1,48 @@
+package testcases;
+
+
+import freemarker.core.ReturnInstruction;
+
+public class CustomStringCompression {
+
+    public static void main(String[] args) {
+        String input = "aaabbccccdd";
+        String output = compressString(input);
+        System.out.println("Input: " + input + " ");
+        System.out.println("Output: " + output + " ");
+    }
+
+    public static String compressString(String input) {
+        StringBuilder compressed = new StringBuilder();
+        int n = input.length();
+        int count=1;
+        for (int i=1;i<=n;i++)
+        {
+
+            if (i<n && input.charAt(i)==input.charAt(i-1))
+            {
+                count++;
+
+            }
+            else
+            {
+                char c=input.charAt(i-1);
+
+                if(count ==2)
+                {
+                    compressed.append(c).append(2);
+                }
+                else {
+                    for (int j=0;j<count;j++)
+                    {
+                        compressed.append(c);
+                    }
+                }
+                System.out.println(c+": count is "+ count);
+                count=1;
+            }
+        }
+
+        return compressed.toString();
+    }
+}
