30102516d9f625300e4b32d3f9dbd7fddc7e2664
adarshadhunti <83895220+adarshadhunti@users.noreply.github.com>
2026-02-04 19:28:56 +0530

    qa

---
+++ src/test/java/javaoops/Anagrams.java
@@ -0,0 +1,26 @@
+package javaoops;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Anagrams {
+    public static void main(String[] args) {
+        String[] str = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        HashMap<String, HashSet<String>> anagramGroups = new HashMap<>();
+
+        for (String word : str) {
+            char[] charArray = word.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            anagramGroups.putIfAbsent(sortedStr, new HashSet<>());
+            anagramGroups.get(sortedStr).add(word);
+        }
+
+        System.out.println("Anagram groups:");
+        for (HashSet<String> group : anagramGroups.values()) {
+            System.out.println(group);
+        }
+        /*System.out.println(anagramGroups.values());*/
+    }
+}
